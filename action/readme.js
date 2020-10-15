2a7fab940abbfac6e1d904090656abd896326b3e
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-10-15 15:17:17 -0300

    update action, readme handling and test

--- action/readme.js
+++ action/readme.js
@@ -1,12 +1,8 @@
-const core = require('@actions/core');
-
 const { replaceContentByData } = require('./template');
 const { read, write } = require('./file');
 
 exports.updateReadme = async (stats) => {
     const content = await read();
     const newContent = replaceContentByData(content, stats);
-    
-    console.log('=== NEW CONTENT ===');
-    console.log(newContent);
+    await write(newContent);
 }
