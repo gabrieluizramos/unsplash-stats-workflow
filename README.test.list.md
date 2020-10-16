773d754e9259518867c6af184ca079b192b72f68
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-10-16 11:42:21 -0300

    update commit bot

--- README.test.list.md
+++ README.test.list.md
@@ -3,6 +3,4 @@ Just a test while creating data
 
 # Unsplash Stats
 <!-- UNSPLASH-STATS:START -->
-- **Views**: 14,882,611
-- **Downloads**: 68,305
 <!-- UNSPLASH-STATS:END -->
--- src/commit.js
+++ src/commit.js
@@ -1,27 +1,29 @@
+const { promisify } = require('util');
+const childProcess = require('child_process');
 
+const exec = promisify(childProcess.exec);
 
 const {
-  // committerUser,
-  // committerEmail,
-  // commitMessage,
-  // githubToken,
+  committerUser,
+  committerEmail,
+  commitMessage,
+  githubToken,
   repository,
-  // README_PATH
+  README_PATH
 } = require('./config');
 
 exports.updateRepo = async () => {
-  // await exec('git', ['config', '--global', 'user.email', committerEmail]);
-  // if (githubToken) {
-  //   await exec('git', [
-  //     'remote',
-  //     'set-url',
-  //     'origin',
-  //     `https://${githubToken}@github.com/${repository}.git`
-  //   ]);
-  // }
-  // await exec('git', ['config', '--global', 'user.name', committerUser]);
-  // await exec('git', ['add', README_PATH]);
-  // await exec('git', ['commit', '-m', commitMessage]);
-  // await exec('git', ['push']);
-  console.log(repository);
+  await exec('git', ['config', '--global', 'user.email', committerEmail]);
+  if (githubToken) {
+    await exec('git', [
+      'remote',
+      'set-url',
+      'origin',
+      `https://${githubToken}@github.com/${repository}.git`
+    ]);
+  }
+  await exec('git', ['config', '--global', 'user.name', committerUser]);
+  await exec('git', ['add', README_PATH]);
+  await exec('git', ['commit', '-m', commitMessage]);
+  await exec('git', ['push']);
 };
