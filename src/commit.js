5dc8f9ca87fefdeb9a7899bee58bad88e1a89c5e
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-10-16 11:56:30 -0300

    update commit bot

--- src/commit.js
+++ src/commit.js
@@ -1,29 +1,45 @@
-const { promisify } = require('util');
-const childProcess = require('child_process');
+const core = require('@actions/core');
+const simpleGit = require('simple-git');
 
-const exec = promisify(childProcess.exec);
+const options = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+const gitClient = simpleGit(options);
 
 const {
   committerUser,
   committerEmail,
   commitMessage,
-  githubToken,
-  repository,
+  // githubToken,
+  // repository,
   README_PATH
 } = require('./config');
 
 exports.updateRepo = async () => {
-  await exec('git', ['config', '--global', 'user.email', committerEmail]);
-  if (githubToken) {
-    await exec('git', [
-      'remote',
-      'set-url',
-      'origin',
-      `https://${githubToken}@github.com/${repository}.git`
-    ]);
-  }
-  await exec('git', ['config', '--global', 'user.name', committerUser]);
-  await exec('git', ['add', README_PATH]);
-  await exec('git', ['commit', '-m', commitMessage]);
-  await exec('git', ['push']);
+  console.log('branch is', core.getInput('branch'));
+  const git = await gitClient.init();
+  await git
+    .addConfig('user.email', committerEmail)
+    .addConfig('user.name', committerUser)
+    .fetch()
+    .pull()
+    .add(README_PATH)
+    .commit(commitMessage)
+    .push()
+  // await exec('git', ['config', '--global', 'user.email', committerEmail]);
+  // if (githubToken) {
+  //   await exec('git', [
+  //     'remote',
+  //     'set-url',
+  //     'origin',
+  //     `https://${githubToken}@github.com/${repository}.git`
+  //   ]);
+  // }
+  // await exec('git', ['config', '--global', 'user.name', committerUser]);
+  // await exec('git', ['add', README_PATH]);
+  // await exec('git', ['commit', '-m', commitMessage]);
+  // await exec('git', ['push']);
 };
